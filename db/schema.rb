64b618b9394c718da8df72d2f1a8b8889c3a0f44
Andrea Mazzini <andrea.mazzini@gmail.com>
2013-10-14 15:09:56 +0200

    Fixed user migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130722085210) do
+ActiveRecord::Schema.define(version: 20131014082438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,15 +26,16 @@ ActiveRecord::Schema.define(version: 20130722085210) do
     t.datetime "updated_at"
     t.integer  "deezer_id"
     t.string   "cover"
+    t.datetime "date"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
