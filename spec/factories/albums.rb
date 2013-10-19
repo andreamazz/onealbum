6f6377022b4a7b62e5593b60361752eb419d4740
Andrea Mazzini <andrea.mazzini@gmail.com>
2013-10-19 19:50:13 +0200

    Added faker

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,9 +1,11 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :album do
-    title 'Texas Flood'
-    author 'Stevie Ray Vaughan and Double Trouble'
-    rating 5
-    text 'Amazing album'
+    title { Faker::Company.catch_phrase }
+    author { Faker::Name.name }
+    rating { Random.rand(CONSTANTS[:rating][:min]..CONSTANTS[:rating][:max]) }
+    text { Faker::Lorem.paragraph }
     sequence(:date) { |n| Date.today + n.days }
     cover 'http://api.deezer.com/2.0/album/6227255/image'
     deezer_id '6227255'
